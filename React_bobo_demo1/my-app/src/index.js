ff461e014629fe214faea729cdc1e593d6687426
dannel <2912672528@qq.com>
2025-03-29 18:46:18 +0800

    配置好环境，在页面显示欢迎

---
+++ React_bobo_demo1/my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>你好，欢迎使用React！</div>
+  </React.StrictMode>
+);
+
